043215935843e17791b03a378e13873d0cabc2fc
Matias Aschoff <aschoffmatias@gmail.com>
2023-08-01 12:41:40 -0300

    Cambios para la segunda pre entrega

--- app.js
+++ app.js
@@ -1,149 +1,186 @@
+//CARRITO DE COMPRAS PARA UNA TIENDA DE PRODUCTOS TECNOLÓGICOS:
+
 // Simulador interactivo de una tienda que le permite al usuario:
 // Escoger productos del listado de la tienda
 // Informar los productos seleccionados
+// Eliminar productos del carrito de compras
 // Calcular el valor total de los productos seleccionados
 // Calcularle el iva
 // Calcular un descuento si la compra supera un monto en específico
-// Calcular las cuotas sin interés que ofrece la tienda
 
 
-// PRECIOS DE LOS PRODUCTOS DE LA TIENDA
-const precioSmartTv = 110000;
-const precioTablet = 140000;
-const precioAuriculares = 160000;
-const precioSmartWatch = 175000;
-const precioCelular = 135000;
-const precioLaptop = 436000;
+// PRODUCTOS  (DESCRIPCION - RESUMEN DE COMPRA - AUMENTAR LA CANTIDAD) 
+class Producto{
+    constructor(id,nombre, precio, cantidad){
+        this.id = id
+        this.nombre = nombre
+        this.precio = precio
+        this.cantidad = cantidad
+    }
 
-// SELECCIONADORES DE LA LISTA DE PRODUCTOS
-const opcion1 = 1;
-const opcion2 = 2;
-const opcion3 = 3;
-const opcion4 = 4;
-const opcion5 = 5;
-const opcion6 = 6;
+    descripcion(){
+        return  this.id+ "-" +
+                this.nombre+
+                "      $" + this.precio
+    }
 
-// DESCRIPCIÓN DE LOS PRODUCTOS DE LA TIENDA
-const smartTv = "Smart tv Philips 6900"
-const tablet = "Tablet Lenovo tab M10"
-const auriculares  = "Auriculares Sony 1000X"
-const smartWatch = "Smart Watch Samsung 5pro"
-const celular = "Celular Moto G72"
-const laptop = "Notebook Lenovo V15"
+    resumenCompra(){
+        return  this.nombre+
+                "$" + this.precio+
+                "   Cantidad: "+ this.cantidad
+    }
 
-// OTRAS VARIABLES GLOBALES
-let rta = ""
-let resultado = 0 
-let ticket = "\n"
+    aumentarCantidad(cantidad){
+        this.cantidad =  this.cantidad + cantidad
+    }
+}
+
+// CONTROLADOR DE PRODUCTOS (AGREGAR - BUSCAR - MOSTRAR)
+class ControladorDeProductos{
+    constructor(){
+        this.listaProductos = []
+    }
+
+    agregar(producto){
+        this.listaProductos.push(producto)
+    }
+
+    buscarProductoPorID(id){
+        return this.listaProductos.find(producto => producto.id == id)
+    }
+
+    mostrarProductos(){
+        let listaEnTexto = ""
+        this.listaProductos.forEach( producto => {
+            listaEnTexto = listaEnTexto + producto.descripcion() +
+            "\n-----------------------------------------------------\n"})
+        alert(listaEnTexto)
+    }
+}
+
+//CARRITO DE COMPRAS  (AGREGAR - MOSTRAR - TOTAL - IVA - DESCRIPCIÓN - DESCUENTO - ELIMINAR PRODUCTOS)
+class Carrito{
+    constructor(){
+        this.listaCarrito = []
+    }
+    
+    agregar(producto,cantidad){
+        let existe = this.listaCarrito.some( elementos => elementos.id == producto.id)
+        if(existe){
+            producto.aumentarCantidad(cantidad)
+        }else{
+            producto.aumentarCantidad(cantidad)
+            this.listaCarrito.push(producto)
+        }
+    }
+
+    mostrarProductos(){
+        let listaEnTexto = "Carrito de compras de Tienda TECNO:\n\n"
+        this.listaCarrito.forEach(producto => {
+            listaEnTexto = listaEnTexto + producto.resumenCompra() + "\n-----------------------------------------------------\n"
+
+        })
+        alert(listaEnTexto)
+    }
+
+    calcularTotal(){
+        return this.listaCarrito.reduce( (acumulador,producto) => acumulador + producto.precio * producto.cantidad ,0)
+    }
+
+    calcularIVA(){
+        return this.calcularTotal() * 1.21
+    }
+
+    calcularDescuento() {
+    const totalConIVA = this.calcularIVA();
+    if (totalConIVA > 500000) {
+        const descuento = totalConIVA * 0.1;
+        const totalConDescuento = totalConIVA - descuento;
+        return totalConDescuento;
+    } else {
+        return totalConIVA;
+    }
+}
+
+    eliminarProductoSeleccionado(id) {
+        const indice = this.listaCarrito.findIndex(producto => producto.id === id);
+        if (indice !== -1) {
+            const productoEliminado = this.listaCarrito.splice(indice, 1);
+            alert(`"El producto: "${productoEliminado[0].nombre}" fue eliminado del carrito.`);
+        } else {
+            alert(`No se encontró ningún producto con ID ${id} en el carrito.`);
+        }
+    }
+}
 
 // FUNCION PARA SALUDAR
 function saludar(){
     alert ("\n* * * * * * * * * * * * * * * * * * * * * * * * *"+
     "\n* * * Bienvenid@ a TIENDA TECNO * * *"+
-    "\n* * * * * * * * * * * * * * * * * * * * * * * * * \n\n" );
+    "\n* * * * * * * * * * * * * * * * * * * * * * * * * \n\n A CONTINUACIÓN PODRÁ SELECCIONAR\n           EL PRODUCTO QUE DESEE" );
 }
-// FUNCION PARA CALCULAR EL IVA
-function iva(resultado){
-    return resultado * 1.21
-}
-// FUNCIONES PARA CALCULAR LAS CUOTAS
-function tresCuotas(resultadoConDescuento){
-    return resultadoConDescuento / 3
-}
-function seisCuotas(resultadoConDescuento){
-    return resultadoConDescuento / 6
-}
-function doceCuotas(resultadoConDescuento){
-    return resultadoConDescuento / 12
-}
+
 // FUNCION PARA DESPEDIR
 function despedir(){
-    alert ("\n\nGRACIAS POR ELEGIR TIENDA TECNO: \n\n Saluda cordialmente Matias Aschoff, Gerente de Ventas")
+    alert ("\nGRACIAS POR ELEGIR TIENDA TECNO: \n\n Saluda cordialmente Matias Aschoff, Gerente de Ventas")
 }
+
+//FUNCION PARA CALCULAR EL DESCUENTO
+function totalDescuento(){
+    const totalConDescuento = CARRITO.calcularDescuento();
+    if (totalConDescuento !== CARRITO.calcularIVA()) {
+        alert("¡Felicitaciones! Su compra supera los $500.000. Ha obtenido un 10% de descuento.");
+        alert("El total de su compra con descuento es de: " + totalConDescuento);
+    }
+}
+
+//CONSTRUCTORES
+const controladorProductos = new ControladorDeProductos()
+const CARRITO = new Carrito()
+
+controladorProductos.agregar(new Producto(1,"Smart tv Philips 6900         ", 110000, 0))
+controladorProductos.agregar(new Producto(2,"Tablet Lenovo tab M10       ", 140000, 0))
+controladorProductos.agregar(new Producto(3,"Auriculares Sony 1000X      ", 160000, 0))
+controladorProductos.agregar(new Producto(4,"Smart Watch Samsung 5pro", 175000, 0))
+controladorProductos.agregar(new Producto(5,"Celular Motorola G72          ", 135000, 0))
+controladorProductos.agregar(new Producto(6,"Notebook Lenovo V15         ", 436000, 0))
+controladorProductos.agregar(new Producto(7,"Proyector Samsung            ", 330000, 0))
+controladorProductos.agregar(new Producto(8,"Asistente Amazon 5th Gen  ", 55000, 0))
+controladorProductos.agregar(new Producto(9,"Samsung s23 ultra               ", 610000, 0))
+controladorProductos.agregar(new Producto(10,"Monitor LG ultra Wide        ", 390000, 0))
+
+//DEFINO RESPUESTA COMO VACIO
+let rta
 
 //SALUDO A TRAVES DE UNA FUNCION
 saludar ()
 
-//BUCLE - CONDICIONALES - VARIABLE LOCAL
+//BUCLE DO - WHILE Y CONDICIONALES
 do{
-    alert ("Seleccione la opción del producto que deseas comprar:\n\n"+opcion1+"-"+smartTv+"\t$"+ 
-    precioSmartTv+"\n"+opcion2+"-"+tablet+"\t$"+ precioTablet+
-    "\n"+opcion3+"-"+auriculares+"\t$"+ precioAuriculares+
-    "\n"+opcion4+"-"+smartWatch+"\t$"+ precioSmartWatch+
-    "\n"+opcion5+"-"+celular+"\t$"+ precioCelular+
-    "\n"+opcion6+"-"+laptop+"\t$"+ precioLaptop);    
+    controladorProductos.mostrarProductos()
+    let opcion = Number(prompt("Ingrese el NÚMERO del producto que desea agregar al carrito"))
+    let producto = controladorProductos.buscarProductoPorID(opcion)
 
-    let opcionElegida = Number(prompt("Ingrese el número que corresponde a la opción"))
-    
-    if(opcionElegida==opcion1){
-    alert ("Usted compro un "+smartTv+"--> su precio es: $"+precioSmartTv)
-
-    resultado = resultado + precioSmartTv 
-    ticket = ticket +"\n" + smartTv +"\t$"+ precioSmartTv
+    if (!producto) {
+        alert("El producto seleccionado no existe. Por favor seleccione un NÚMERO VÁLIDO");
+        continue;
     }
-    else if(opcionElegida==opcion2){
-    alert ("Usted compro una "+tablet+"--> su precio es: $"+precioTablet)
-    
-    resultado = resultado + precioTablet
-    ticket = ticket +"\n" + tablet +"\t$"+ precioTablet
+    let cantidad = Number(prompt("Ingrese la CANTIDAD del producto seleccionado que desea"))
+    CARRITO.agregar(producto,cantidad)
+    alert("El producto fué añadido exitosamente: ")
+
+    rta = prompt("¿Desea ELIMINAR algún producto del carrito? Si/No").toLowerCase();
+    if (rta === "si") {
+        let idProductoAEliminar = Number(prompt("Ingrese el ID del producto que desea eliminar del carrito"));
+        CARRITO.eliminarProductoSeleccionado(idProductoAEliminar);
+        CARRITO.mostrarProductos();
     }
 
-    else if(opcionElegida==opcion3){
-    alert ("Usted compro una "+auriculares+"--> su precio es: $"+precioAuriculares)
-    
-    resultado = resultado + precioAuriculares
-    ticket = ticket +"\n" + auriculares +"\t$"+ precioAuriculares
-    }
-    else if(opcionElegida==opcion4){
-    alert ("Usted compro una "+smartWatch+"--> su precio es: $"+precioSmartWatch)
-    
-    resultado = resultado + precioSmartWatch
-    ticket = ticket +"\n" + smartWatch +"\t$"+ precioSmartWatch
-    }
-    else if(opcionElegida==opcion5){
-    alert ("Usted compro una "+celular+"--> su precio es: $"+precioCelular)
-    
-    resultado = resultado + precioCelular
-    ticket = ticket +"\n" + celular +"\t$"+ precioCelular
-    }
-    else if(opcionElegida==opcion6){
-    alert ("Usted compro una "+laptop+"--> su precio es: $"+precioLaptop)
-    
-    resultado = resultado + precioLaptop
-    ticket = ticket +"\n" + laptop +"\t$"+ precioLaptop
-    }
-    // SI NO SE ELIGE NINGUNA DE LAS OPCIONES, MOSTRAR EL SIGUIENTE MENSAJE
-    else if(opcionElegida!=opcion1 && opcion2 && opcion3 && opcion4 && opcion5 && opcion6){
-    alert("La opción escogida NO es válida, por favor intente de nuevo")
-    }
-    //PARA SALIR DEL BUCLE
+    CARRITO.mostrarProductos()
     rta = prompt("¿Desea salir? Para salir escriba 'salir'").toLowerCase()
 }while(rta != "salir")
-//MOSTRAR TOTAL DE COMPRA
-alert ("Usted compro: "+ticket+"\n\n El total de su compra es: $"+resultado)
 
-//DESCUENTO DEL 10% PARA COMPRAS SUPERIORES A $180.000
-if(resultado >= 180000){
-    let resultadoConDescuento = iva(resultado)* 0.9
-    
-    alert("GRACIAS POR COMPRAR EN TIENDA TECNO: \n\n RESUMEN DE COMPRA: "+ticket 
-        +"\n\nTotal: $" + resultado
-        +"\n\n"+"El total con IVA es de: $"+iva(resultado)+"\n\nTotal con descuento: $" + resultadoConDescuento)
-    
-    alert("Tambien podrá abonar su compra en:\n\n --> 3 cuotas sin interes de: $"+tresCuotas(resultadoConDescuento)+
-        "\n\n --> 6 cuotas sin interes de: $"+seisCuotas(resultadoConDescuento)+
-        "\n\n --> 12 cuotas sin interes de: $"+doceCuotas(resultadoConDescuento))
-    despedir()
-}else{
-    alert("Accedé a los DESCUENTOS que TIENDA TECNO tiene para vos \nen COMPRAS SUPERIORES a $180000")
-    
-    let resultadoConDescuento = iva(resultado)* 0.9
-    
-    alert("\n RESUMEN DE COMPRA: "+ticket 
-        +"\n\nTotal: $" + resultado
-        +"\n\n"+"El total con IVA es de: $"+iva(resultado)+"\n\nTotal con descuento: $" + resultadoConDescuento)
-    
-        despedir()
-}
-
-
+alert("El total de su compra es de: "+ CARRITO.calcularTotal())
+alert("El total de su compra con IVA es de: "+ CARRITO.calcularIVA())
+totalDescuento ()
+despedir()
